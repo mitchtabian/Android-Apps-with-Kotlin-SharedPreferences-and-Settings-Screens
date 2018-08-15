8282da09c9ddcaad2eef1af6843739b5ad661338
Mitch Tabian <mitch@tabian.ca>
2018-08-15 12:46:38 -0700

    end

--- app/src/main/java/com/jwhh/notekeeper/AccountActivity.kt
+++ app/src/main/java/com/jwhh/notekeeper/AccountActivity.kt
@@ -1,8 +1,11 @@
 package com.jwhh.notekeeper
 
+import android.content.SharedPreferences
 import android.os.*
+import android.preference.PreferenceManager
 import android.support.v7.app.AppCompatActivity
 import android.telephony.PhoneNumberFormattingTextWatcher
+import android.util.Log
 import android.view.View
 import kotlinx.android.synthetic.main.activity_account.*
 import kotlinx.android.synthetic.main.layout_account_toolbar.*
@@ -19,6 +22,32 @@ class AccountActivity : AppCompatActivity(),
         input_phone_number.addTextChangedListener(PhoneNumberFormattingTextWatcher(Locale.getDefault().country))
 
         initToolbar()
+        initWidgetValues()
+    }
+
+    private fun initWidgetValues(){
+        val prefs: SharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
+
+        val name = prefs.getString(PREFERENCES_NAME, "Mitch Tabian")
+        val username = prefs.getString(PREFERENCES_USERNAME, "Mitch.Tabian")
+        val email = prefs.getString(PREFERENCES_EMAIL, "mitch@tabian.ca")
+        val phoneNum = prefs.getString(PREFERENCES_PHONE_NUMBER, "1 604 855-1111")
+        val gender = prefs.getString(PREFERENCES_GENDER, "Male")
+
+        Log.d("name", name)
+
+        input_name.setText(name)
+        input_username.setText(username)
+        input_email_address.setText(email)
+        input_phone_number.setText(phoneNum)
+
+        val genders = resources.getStringArray(R.array.gender_array)
+        for (index in 0.. genders.size){
+            if(genders.get(index).equals(gender)){
+                gender_spinner.setSelection(index)
+                break
+            }
+        }
     }
 
     override fun onClick(widget: View?) {
