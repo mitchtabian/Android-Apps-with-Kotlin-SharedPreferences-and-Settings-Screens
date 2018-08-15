f5eb1f986d44f7074b67f36d98ecc02779ef5f3e
Mitch Tabian <mitch@tabian.ca>
2018-08-15 13:07:07 -0700

    end

--- app/src/main/java/com/jwhh/notekeeper/SettingsActivity1Fragment.kt
+++ app/src/main/java/com/jwhh/notekeeper/SettingsActivity1Fragment.kt
@@ -2,11 +2,13 @@ package com.jwhh.notekeeper
 
 
 import android.os.Bundle
+import android.preference.Preference
 import android.preference.PreferenceFragment
 import android.util.Log
 
 
-class SettingsActivity1Fragment : PreferenceFragment()
+class SettingsActivity1Fragment : PreferenceFragment(),
+        Preference.OnPreferenceClickListener
 {
 
     val TAG = "Settings1Fragment"
@@ -14,9 +16,21 @@ class SettingsActivity1Fragment : PreferenceFragment()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         addPreferencesFromResource(R.xml.pref_main)
-        
+
+        // Set Preference Click Listener
+        val uploadWifiPreference: Preference = preferenceManager.findPreference(getString(R.string.key_upload_over_wifi))
+        uploadWifiPreference.onPreferenceClickListener = this
     }
-    
+
+    override fun onPreferenceClick(preference: Preference?): Boolean {
+        when(preference!!.key){
+            getString(R.string.key_upload_over_wifi) -> {
+                printToLog("Change detected to preference. key: " + getString(R.string.key_upload_over_wifi))
+            }
+        }
+
+        return true
+    }
 
     private fun printToLog(message: String?){
         Log.d(TAG, message)
