6d444dcf018a25e646f87b018c855d04a80bd5fd
Mitch Tabian <mitch@tabian.ca>
2018-08-15 13:02:57 -0700

    start

--- app/src/main/java/com/jwhh/notekeeper/SettingsActivity1Fragment.kt
+++ app/src/main/java/com/jwhh/notekeeper/SettingsActivity1Fragment.kt
@@ -6,11 +6,19 @@ import android.preference.PreferenceFragment
 import android.util.Log
 
 
-class SettingsActivity1Fragment : PreferenceFragment() {
+class SettingsActivity1Fragment : PreferenceFragment()
+{
 
+    val TAG = "Settings1Fragment"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         addPreferencesFromResource(R.xml.pref_main)
+        
+    }
+    
+
+    private fun printToLog(message: String?){
+        Log.d(TAG, message)
     }
 }
